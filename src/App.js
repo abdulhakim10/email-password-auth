8868cc132739740be0b625813183417707ea3d31
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-18 01:48:34 +0600

    Simple form, input, change, blur, submit, preventDefault added

--- src/App.js
+++ src/App.js
@@ -4,14 +4,30 @@ import app from './firebase/firebase.init';
 
 
 const auth = getAuth(app);
+const handleRegister = event => {
+  event.preventDefault();
+  const email = event.target.email.value;
+  const password = event.target.password.value;
+  console.log(email, password)
+}
+
+const handleEmailBlur = event => {
+  console.log(event.target.value);
+}
+
+const handlePasswordChange = event => {
+  console.log(event.target.value);
+}
 
 function App() {
   return (
     <div className="App">
-      <form>
-        <input type="email" name="" id="" placeholder='Your Email' />
+      <form onSubmit={handleRegister}>
+        <input onBlur={handleEmailBlur} type="email" name="email" id="" placeholder='Your Email' />
         <br />
-        <input type="password" name="" id="" placeholder='Your Password' />
+        <input onChange={handlePasswordChange} type="password" name="password" id="" placeholder='Your Password' />
+        <br />
+        <button type="submit">Register</button>
       </form>
     </div>
   );
--- src/_steps.js
+++ src/_steps.js
@@ -3,3 +3,10 @@
 'log in' === 'sign in' [Already you have a user account created]
 
 */
+
+/*
+    get form value:
+    set form onSubmitHandler
+    set event parameter
+    event.target.name.value
+*/
