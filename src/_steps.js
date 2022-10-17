5b066f1cc7ef801a775fe1b4dd66e2004fe64c5b
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-18 00:18:36 +0600

    Login, Registration, Sign up, Sign in, project setup

---
+++ src/_steps.js
@@ -0,0 +1,5 @@
+/*
+'register' or 'sign up' [First time create a user] 
+'log in' === 'sign in' [Already you have a user account created]
+
+*/
---
+++ src/firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDtIONyjbESONKWNtITzNe0OgINhy2kBno",
+  authDomain: "email-password-auth-2c76d.firebaseapp.com",
+  projectId: "email-password-auth-2c76d",
+  storageBucket: "email-password-auth-2c76d.appspot.com",
+  messagingSenderId: "78530452701",
+  appId: "1:78530452701:web:ee7c36ddea3d4736f10f34"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
