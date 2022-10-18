5248e946a0c8af6805197e9276ee75a4f59f3ec7
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-10-19 00:38:35 +0600

    reset password added

--- src/components/LoginBootstrap.js
+++ src/components/LoginBootstrap.js
@@ -1,4 +1,4 @@
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import app from '../firebase/firebase.init';
@@ -8,6 +8,7 @@ const auth = getAuth(app);
 const LoginBootstrap = () => {
 
   const [success, setSuccess] = useState(false);
+  const [userEmail, setUserEmail] = useState('');
 
   const handleSubmit = event => {
     setSuccess(false);
@@ -17,6 +18,7 @@ const LoginBootstrap = () => {
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password)
+
     signInWithEmailAndPassword(auth, email, password)
     .then( result => {
       const user = result.user;
@@ -29,13 +31,33 @@ const LoginBootstrap = () => {
     })
     
   }
+
+  const handleEmailBlur = event => {
+    const email = event.target.value;
+    setUserEmail(email);
+  }
+
+  const handleResetPassword = () => {
+
+    if(!userEmail){
+      alert('Please enter your email address');
+      return;
+    }
+    sendPasswordResetEmail(auth, userEmail)
+    .then(() => {
+      alert('Password reset email sent. Please check your email')
+    })
+    .catch( error => {
+      console.error('error', error);
+    })
+  }
     return (
         <div className='w-50 mx-auto'>
             <h2 className='text-primary'>Please Log in</h2>
        <form onSubmit={handleSubmit}>
        <div className="mb-3">
   <label htmlFor="formGroupExampleInput" className="form-label">Email Address</label>
-  <input type="email" name='email' className="form-control" id="formGroupExampleInput" placeholder="Your Email"/>
+  <input type="email" name='email' onBlur={handleEmailBlur} className="form-control" id="formGroupExampleInput" placeholder="Your Email"/>
 </div>
 <div className="mb-3">
   <label htmlFor="formGroupExampleInput2" className="form-label">Password</label>
@@ -47,6 +69,7 @@ const LoginBootstrap = () => {
   <button type="submit" className="btn btn-primary">Login</button>
        </form>
        <p><small>New to this website? Please <Link to='/register'>Register</Link></small></p>
+       <p><small>Forget Password<button type="button" onClick={handleResetPassword} class="btn btn-link"><small>Reset Password</small></button></small></p>
         </div>
     );
 };    
